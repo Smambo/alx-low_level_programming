6601553c703cd2dc28b195cec60b4587273114f6
Simamnkele <smambo85@gmail.com>
2023-03-17 16:00:37 -0700

    Trying again

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,7 +21,7 @@ int main(void)
 			{
 				putchar((n % 10) + '0');
 				putchar((m % 10) + '0');
-				if (n != 9 || m != 10)
+				if (n != 8 || m != 9)
 				{
 					putchar(',');
 					putchar(' ');
