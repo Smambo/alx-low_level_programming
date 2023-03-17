c764a3e673741f5e4776adc1e5030151575f08b7
Simamnkele <smambo85@gmail.com>
2023-03-17 16:17:04 -0700

    Program prints all possible different combos of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int p;
+	int q;
+	int r;
+
+	for (p = 0; p < 10; p++)
+	{
+		for (q = 0; q < 10; q++)
+		{
+			for (r = 0; r < 10; r++)
+			{
+				if ((r % 10) > (q % 10) > (p % 10))
+				{
+					putchar((p % 10) + '0');
+					putchar((q % 10) + '0');
+					putchar((r % 10) + '0');
+					if (p != 7 || q != 8 || r != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
