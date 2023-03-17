ce261f06694cc68f5837d4333eeeb6c60e892b96
Simamnkele <smambo85@gmail.com>
2023-03-17 10:34:51 -0700

    Fixing checker errors

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,9 +13,12 @@ int main(void)
 
 {
 	int n;
+	int lastn;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	lastn = n % 10;
+
 	if (n > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
