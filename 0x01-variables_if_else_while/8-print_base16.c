2ef43c955aa1e9c97c377e96e4fc589a070fc717
Simamnkele <smambo85@gmail.com>
2023-03-17 13:07:13 -0700

    Program prints all numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 0; n < 16; n++)
+	{
+		putchar('0' + n);
+	}
+	putchar('\n');
+	return (0);
+}
