8e914a7db3092ff2901f9260ecc80cedba2caaf5
Simamnkele <smambo85@gmail.com>
2023-03-20 20:52:54 +0000

    Function prints alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * print_alphabet - does just that
@@ -6,7 +6,7 @@
  * Return: 0 (Success)
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 
 {
 	char a;
