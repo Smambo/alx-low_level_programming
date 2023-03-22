872b7a31e328321df65d92f4aa191db67cad77fd
Simamnkele <smambo85@gmail.com>
2023-03-22 10:01:59 +0000

    Fixing compilation error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,35 +11,23 @@ void print_times_table(int n)
 {
 	int row;
 	int col;
+	int prod = row * col;
 
 	if (n > 15 || n < 0)
 	{
 		return;
 	}
-
 	for (row = 0; row <= n; row++)
-
+	{
 		for (col = 0; col <= n; col++)
 		{
-			if (col == 0)
+			printf("%d", prod);
+			if (col != n)
 			{
-				_putchar('0');
-			}
-			else if (row * col <= n)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((row * col) + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((row * col) / 10 + '0');
-				_putchar((row * col) % 10 + '0');
+				printf(", ");
 			}
 		}
-		_putchar('\n');
+		printf("\n");
 	}
+
 }
