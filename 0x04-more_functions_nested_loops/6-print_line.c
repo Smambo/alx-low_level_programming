940729e52f761d92bdc2d805b316534027d10ee9
Simamnkele <smambo85@gmail.com>
2023-03-23 16:34:08 +0000

    Fixed output error

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,16 +10,15 @@ void print_line(int n)
 {
 	int a;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
 	for (a = 0; a <= n; a++)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar('_');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
