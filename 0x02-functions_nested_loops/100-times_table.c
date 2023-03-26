902f881aa0cf29b7bb099a5fe0c7a2389668d8c6
Simamnkele <smambo85@gmail.com>
2023-03-26 17:59:06 +0000

    Fixed spacing error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,22 +5,19 @@
  * print_times_table - prints the n times table starting with 0
  *
  * @n: number to determine the times table
+ *
  */
 
 void print_times_table(int n)
 {
-	int row;
-	int col;
+	int row, col;
 
 	if (n > 15 || n < 0)
-	{
 		return;
-	}
 	for (row = 0; row <= n; row++)
 	{
 		for (col = 0; col <= n; col++)
 		{
-
 			int prod = row * col;
 
 			if (col == 0)
@@ -35,7 +32,7 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(prod + '0');
 			}
-			else if (prod <=99)
+			else if (prod <= 99)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -54,5 +51,4 @@ void print_times_table(int n)
 		}
 		_putchar('\n');
 	}
-
 }
