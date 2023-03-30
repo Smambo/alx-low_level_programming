b9fc8340b5975d8719276a5ab42089168239cc25
Simamnkele <smambo85@gmail.com>
2023-03-30 22:39:40 +0000

    Fixed errors

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,44 @@
 #include "main.h"
 /**
+ * _indexOf - returns boolean if special char is detected
+ *
+ * @a: character to be checked
+ *
+ * Return: 1 if true, 0 if false
+ */
+int _indexOf(char a)
+{
+	int i;
+	char arr[13] = {' ','\t','\n',',',';','.','!','?','"','(',')','{','}'};
+
+	for (i = 0; i < 13; i++)
+	{
+		if (arr[i] == a)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+/**
  *cap_string - capitalizes all words of a string
  *
  * @str: string to be capitalized
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+		if (_indexOf(str[i]))
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32;
-			}
+			continue;
 		}
-		else
+		if (str[i] >= 'a' && str[i] <= 'z' && (_indexOf(str[i - 1]) || i == 0))
 		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-			{
-				str[i] = str[i] + 32;
-			}
+			str[i] = str[i] -32;
 		}
-
-		i++;
 	}
-
-	return str;
+	return (str);
 }
