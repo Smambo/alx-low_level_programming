c7cbfe536836e6be51b817c9dddd81aeec5037f0
Simamnkele <smambo85@gmail.com>
2023-04-14 11:24:28 +0000

    Fixed output error

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 
-	ptr = malloc(sizeof(char) * len1 + n + 1);
+	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (ptr == NULL)
 		return (NULL);
 
