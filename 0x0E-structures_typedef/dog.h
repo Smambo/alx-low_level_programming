c21b1ed7c960d66d0a8cb0b2b28a6b6af7e2a973
Simamnkele <smambo85@gmail.com>
2023-04-17 20:43:52 +0000

    Updated header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,4 +13,8 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 #endif /* _DOG_H */
