1b8f78c6e63da3126c2f5bd17fa0f88b2dcca744
Simamnkele <smambo85@gmail.com>
2023-04-18 21:12:53 +0000

    Fixed Betty coding style issues

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,26 +17,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *ptr;
 
 	for (i = 0; name[i] != '\0'; i++)
-	{
 		len_name++;
-	}
+
 	for (i = 0; owner[i] != '\0'; i++)
-	{
 		len_own++;
-	}
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
-
 	ptr_n = malloc(sizeof(char) * (len_name + 1));
 	if (ptr_n == NULL)
+	{
 		free(ptr);
 		return (NULL);
+	}
 	ptr_o = malloc(sizeof(char) * (len_own + 1));
 	if (ptr_o == NULL)
+	{
 		free(ptr_n);
 		free(ptr);
 		return (NULL);
+	}
 	for (i = 0; i < len_name; i++)
 	{
 		ptr_n[i] = name[i];
