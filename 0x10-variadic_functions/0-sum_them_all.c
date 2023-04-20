679b68b666e407556cd53fea2eab33bef3fea249
Simamnkele <smambo85@gmail.com>
2023-04-20 19:18:54 +0000

    Fixed Betty coding style error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,13 +13,13 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	va_start (ap, n);
+	va_start(ap, n);
 
 	sum = 0;
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg (ap, int);
+		sum += va_arg(ap, int);
 	}
-	va_end (ap);
+	va_end(ap);
 	return (sum);
 }
