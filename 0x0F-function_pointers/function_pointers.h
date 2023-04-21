71e121201a149209a8fe197f4b8c516bda30445c
Simamnkele <smambo85@gmail.com>
2023-04-21 10:42:08 +0000

    Fixed checker errors

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,4 @@
-#ifndef STDDEF_H
-#define STDDEF_H
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-#endif /* STDDEF_H */
-
+#include <stddef.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 int _putchar(char c);
