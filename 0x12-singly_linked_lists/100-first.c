bb9289d55ca51e390cf2221b5c450dffac466c74
Simamnkele <smambo85@gmail.com>
2023-04-29 16:55:47 +0000

    Added function that prints string before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "100-main.c"
+/**
+ * _first - prints a string before main function is executed
+ */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");	
+}
