82a00f4d7b52ba1715c50a74669863c279335aa1
Simamnkele <smambo85@gmail.com>
2023-04-29 14:30:52 +0000

    Fixed output error

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,17 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * is_palindrome - checks if string is a palindrome
- *
- * @s: input string
- *
- * Return: 1 if string is a palindrome, else 0
+ * palindrome_iterator - iterates through str to check if palindrome
+ * @s: string to be iterated through
+ * @start: index of start of string
+ * @end: index of end of string
+ * Return: 1 if palindrome, else 0
  */
-int is_palindrome(char *s)
+int palindrome_iterator(char *s, int start, int end)
 {
-	int start = 0;
-	int end = strlen(s - 1);
-
 	if (s[start] != s[end])
 	{
 		return (0);
@@ -21,5 +18,18 @@ int is_palindrome(char *s)
 	{
 		return (1);
 	}
-	return (is_palindrome(s));
+	return (palindrome_iterator(s, start + 1, end - 1));
+}
+/**
+ * is_palindrome - checks if string is a palindrome
+ *
+ * @s: input string
+ *
+ * Return: 1 if string is a palindrome, else 0
+ */
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+
+	return (palindrome_iterator(s, 0, len - 1));
 }
