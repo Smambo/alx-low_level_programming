e31ad4fd0fcc1d8c208d72346aeea90aca3dd425
Simamnkele <smambo85@gmail.com>
2023-05-03 11:59:49 +0000

    Added function that frees list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list
+ * @head: head of the list
+ * Return: nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	if (head == NULL)
+	{
+		return;
+	}	
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
