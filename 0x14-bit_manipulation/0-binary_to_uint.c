649031c868d24165fdd11bf1f8bc58bb3b633d27
Simamnkele <smambo85@gmail.com>
2023-05-05 23:39:48 +0000

    Fixed output error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,6 +10,11 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int result;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
+
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
