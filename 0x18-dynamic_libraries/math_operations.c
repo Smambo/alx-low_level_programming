9a2c8823407590153ade8a2c612ee4897a4aa4ed
Simamnkele <smambo85@gmail.com>
2023-08-21 19:40:07 +0200

    Calling dynamic library from Python advanced task

---
+++ 0x18-dynamic_libraries/math_operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - sum of two integers
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - difference between two integers
+ * @a: first int
+ * @b: second int
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - product of two integers
+ * @a: first int
+ * @b: second int
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - quotient of two integers
+ * @a: first int
+ * @b: second int
+ * Return: quotient
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - remainder from division of two integers
+ * @a: first int
+ * @b: second int
+ * Return: modulo quotient
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
