e5d6831e9c8f19eda4ec117efebd098bfc813ae0
Simamnkele <smambo85@gmail.com>
2023-09-08 19:14:48 +0200

    Added function that returns index of a key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+#include "1-djb2.c"
+/**
+ * key_index - returns the index of a key
+ * @key: the key in the array
+ * @size: size of the array
+ * Return: index at which key/value pair should be
+ * stored in array
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
