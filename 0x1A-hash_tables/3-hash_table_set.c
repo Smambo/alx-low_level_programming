75d863fd2087eea6b1271b749fc7323dea09fa70
Simamnkele <smambo85@gmail.com>
2023-09-13 19:31:21 +0200

    Updated to fix checker errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,6 +28,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		check = check->next;
 	}
 	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
 	if (!new->key)
 	{
 		free(new);
